276dc8e57a66d2e2ec83659421a50d10f0b88189
jinkim-tech <jhkim8268@gmail.com>
2019-12-31 16:09:33 -0800

    Initial commit

---
+++ test/unit/mortgage.spec.js
@@ -0,0 +1,19 @@
+const expect = require('chai').expect;
+const Mortgage = require('../../src/js/lib/Mortgage.js');
+
+describe('Mortgage Calculator', () => {
+  let mortgageCalculator = null;
+  
+  beforeEach(() => {
+    mortgageCalculator = new Mortgage(420000, 3.75, 30, 12);
+  });
+
+  it('should have a monthly payment function', () => {
+    expect(mortgageCalculator.monthlyPayment).to.exist;
+  });
+
+  it('should calculate monthly payment correctly', () => {
+    expect(mortgageCalculator.monthlyPayment()).to.equal(1945.0854846029222);
+  })
+
+});
